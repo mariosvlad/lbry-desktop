473acc0d0f7d18f9ba58b7e4081b8444f74e9607
Sean Yesmunt <sean@lbry.io>
2021-04-14 00:23:25 -0400

    odysee
    fix replay select styling
    
    make meme a link
    
    Fix audio references
    
    get newest livestream claim in livestreamLink
    
    pin crackermilk
    
    fix livestream banner placement
    
    fix live page
    
    fix rebase
    
    fix rebase
    
    fix error nag
    
    fix darkmode blockquote style
    
    break word on livestream comment text
    
    fix dark mode snack
    
    fix live badge
    
    fix lint
    
    small fixes - word wrap, live badge
    
    wip
    
    Fix invisible snack in Odysee Light Theme
    
    Revert "wip"
    
    This reverts commit d17e477fe0e6633709ea30bdc403448825db4c71.
    
    Revert "small fixes - word wrap, live badge"
    
    This reverts commit 0e431d4038d774079c78f0de32238aac7260e4ca.
    
    fix blank
    
    pinned destiny
    
    fix badges and homepage again
    
    only get livestreams live for less than a day
    
    pinned hammy and olivia
    
    multi pin
    
    pin destiny
    
    updated pinned videos
    
    update tagline
    
    Update view.jsx
    
    pins
    
    updated destiny's video
    
    updated pinned videos
    
    removed destiny, added lie likes music
    
    pinned destiny and mason's woodshop
    
    removed hammy and olivia
    
    unpinned mason's woodshop
    
    removed pins

--- ui/component/commentCreate/view.jsx
+++ ui/component/commentCreate/view.jsx
@@ -31,11 +31,15 @@ type Props = {
   isReply: boolean,
   activeChannel: string,
   activeChannelClaim: ?ChannelClaim,
-  livestream?: boolean,
+  bottom: boolean,
+  onSubmit: (string, string) => void,
+  livestream: boolean,
+  embed?: boolean,
   toast: (string) => void,
   claimIsMine: boolean,
   sendTip: ({}, (any) => void, (any) => void) => void,
   setjustCommented: (boolean) => void,
+  commentingEnabled: boolean,
 };
 
 export function CommentCreate(props: Props) {
@@ -50,11 +54,15 @@ export function CommentCreate(props: Props) {
     isReply,
     parentId,
     activeChannelClaim,
+    onSubmit,
+    bottom,
     livestream,
+    embed,
     toast,
     claimIsMine,
     sendTip,
     setjustCommented,
+    commentingEnabled,
   } = props;
   const buttonref: ElementRef<any> = React.useRef();
   const {
@@ -86,7 +94,7 @@ export function CommentCreate(props: Props) {
 
   function altEnterListener(e: SyntheticKeyboardEvent<*>) {
     const KEYCODE_ENTER = 13;
-    if ((e.ctrlKey || e.metaKey) && e.keyCode === KEYCODE_ENTER) {
+    if ((livestream || e.ctrlKey || e.metaKey) && e.keyCode === KEYCODE_ENTER) {
       e.preventDefault();
       buttonref.current.click();
     }
@@ -155,6 +163,10 @@ export function CommentCreate(props: Props) {
           setIsSupportComment(false);
           setjustCommented(true);
 
+          if (onSubmit) {
+            onSubmit(commentValue, activeChannelClaim.name);
+          }
+
           if (onDoneReplying) {
             onDoneReplying();
           }
@@ -169,11 +181,16 @@ export function CommentCreate(props: Props) {
     setAdvancedEditor(!advancedEditor);
   }
 
-  if (!hasChannels) {
+  if (!commentingEnabled || !hasChannels) {
     return (
       <div
         role="button"
         onClick={() => {
+          if (embed) {
+            window.open(`https://odysee.com/$/${PAGES.AUTH}?redirect=/$/${PAGES.LIVESTREAM}`);
+            return;
+          }
+
           const pathPlusRedirect = `/$/${PAGES.CHANNEL_NEW}?redirect=${pathname}`;
           if (livestream) {
             window.open(pathPlusRedirect);
@@ -227,6 +244,7 @@ export function CommentCreate(props: Props) {
       className={classnames('comment__create', {
         'comment__create--reply': isReply,
         'comment__create--nested-reply': isNested,
+        'comment__create--bottom': bottom,
       })}
     >
       <FormField
